4f9b854673c22e36510e11e88afed742c6c0ed62
Yuichi Ishikawa <yuichiis.pub@gmail.com>
2019-05-06 16:02:58 +0900

    Update Bootstrap.php
    create cache directory

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -6,6 +6,7 @@ date_default_timezone_set('UTC');
 include 'init_autoloader.php';
 
 define('RINDOW_TEST_CACHE',     __DIR__.'/cache');
+@mkdir(RINDOW_TEST_CACHE, $mode = 0777 , $recursive = TRUE);
 
 if(!class_exists('PHPUnit\Framework\TestCase')) {
     include __DIR__.'/travis/patch55.php';
